745d1b5507a80d6ad3bf9849a8fb88bcb1512998
Joao Cordeiro <jschmidtcordeiro@gmail.com>
2023-10-25 12:17:32 -0300

    Topological Order

---
+++ Activity 2/2-Ordenacao Topologica/Graph.h
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <list>
+#include <stack>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Graph {
+    int V; // Number of vertices
+    list<int> *adj;
+    unordered_map <int, string> nodes;
+
+    void DFS(int v, bool visited[], vector<int> &scc);
+    void fillOrder(int v, bool visited[], stack<int> &Stack);
+
+public:
+    Graph(int V);
+    void addEdge(int v, int w);
+    void addNode(int id, string label);
+    // Graph getTranspose();
+    void printTopologicalOrder();
+};
+
+Graph::Graph(int V) {
+    this->V = V;
+    adj = new list<int>[V];
+}
+
+void Graph::addEdge(int v, int w) {
+    adj[v - 1].push_back(w - 1);
+}
+
+void Graph::addNode(int id, string label) {
+    nodes.insert({id, label});
+}
+
+void Graph::DFS(int v, bool visited[], vector<int> &scc) {
+    visited[v] = true;
+    scc.push_back(v + 1); // Push the vertex (add 1 for 1-based indexing)
+
+    for (auto i = adj[v].begin(); i != adj[v].end(); ++i)
+        if (!visited[*i])
+            DFS(*i, visited, scc);
+}
+
+// Graph Graph::getTranspose() {
+//     Graph g(V);
+//     for (int v = 0; v < V; v++) {
+//         for (auto i = adj[v].begin(); i != adj[v].end(); ++i) {
+//             g.adj[*i].push_back(v);
+//         }
+//     }
+//     return g;
+// }
+
+void Graph::fillOrder(int v, bool visited[], stack<int> &Stack) {
+    visited[v] = true;
+
+    for (auto i = adj[v].begin(); i != adj[v].end(); ++i)
+        if (!visited[*i])
+            fillOrder(*i, visited, Stack);
+
+    Stack.push(v);
+}
+
+void Graph::printTopologicalOrder() {
+    stack<int> Stack;
+    bool *visited = new bool[V];
+    
+    for (int i = 0; i < V; i++)
+        visited[i] = false;
+
+    for (int i = 0; i < V; i++) {
+        if (!visited[i]) {
+            fillOrder(i, visited, Stack);
+        }
+    }
+
+    while (!Stack.empty()) {
+        cout << nodes[Stack.top() + 1] << " → "; // Print the vertex (add 1 for 1-based indexing)
+        Stack.pop();
+    }
+    cout << endl;
+}
---
+++ Activity 2/2-Ordenacao Topologica/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include "Graph.h"  // Assuming Graph.h contains the declaration of your Graph class
+
+int main() {
+    std::ifstream inputFile;
+    inputFile.open("input");
+
+    if (!inputFile.is_open()) {
+        std::cerr << "Could not open the file." << std::endl;
+        return 1;  // Return an error code
+    }        
+
+    bool readingNodes = true;
+    bool readingEdges = false;
+    
+    int nodeNumber;
+
+
+    std::string line;
+    std::getline(inputFile, line);
+    std::stringstream line_stream(line);
+    std::string label;  // You need to add this variable
+    line_stream >> label >> nodeNumber;  // Corrected line
+
+    Graph g(nodeNumber);  // Create the graph with the correct number of nodes
+
+    while (std::getline(inputFile, line)) {
+        std::stringstream line_stream(line);
+
+        if (line == "*edges") {
+            readingNodes = false;
+            readingEdges = true;
+            continue;
+        }
+
+        if (readingNodes) {
+            int nodeIndex;
+            std::string label;
+            line_stream >> nodeIndex >> label;
+            g.addNode(nodeIndex, label);
+        } else if (readingEdges) {
+            int a, b, weight;
+            line_stream >> a >> b >> weight;
+            // std::cout << a << " " << b << " " << weight << std::endl;
+            g.addEdge(a, b);
+        }
+    }
+
+    g.printTopologicalOrder();
+
+    inputFile.close();
+
+    return 0;
+}
